c5489cb8183067b9adb5202fbb98aa31696827e1
Jolfr <thomas.jack.carroll@gmail.com>
2021-03-01 07:58:22 -0500

    did basic def for trigger block

--- components/TriggerBlock/index.vue
+++ components/TriggerBlock/index.vue
@@ -1,8 +1,23 @@
-<template><div></div></template>
+<template>
+  <div class="trigger">
+    <BasicBlock :label="trigger.name" class="bg-red-700 text-white" />
+  </div>
+</template>
 
 <script>
+import BasicBlock from '../BasicBlock/index'
+
 export default {
   name: 'TriggerBlock',
+  components: [BasicBlock],
+  props: {
+    trigger: {
+      type: Object,
+      required: true,
+      default: () => ({ name: '', ref: '' }),
+      validator: (trigger) => ['name', 'ref'].every((key) => key in trigger),
+    },
+  },
 }
 </script>
 
