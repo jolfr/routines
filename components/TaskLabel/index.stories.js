f560cb4988dbd2c5431cc00666adc05d36d49fed
Jolfr <thomas.jack.carroll@gmail.com>
2021-03-01 08:36:02 -0500

    added component for task label

---
+++ components/TaskLabel/index.stories.js
@@ -0,0 +1,20 @@
+import TaskLabel from './index'
+
+export default {
+  title: 'Task Label',
+  component: TaskLabel,
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+}
+
+const Template = (args, { argTypes }) => ({
+  components: { TaskLabel },
+  props: Object.keys(argTypes),
+  template: '<TaskLabel v-bind="$props"/>',
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  name: 'Test name',
+  description: 'Test description',
+}
---
+++ components/TaskLabel/index.vue
@@ -0,0 +1,9 @@
+<template><div></div></template>
+
+<script>
+export default {
+  name: 'TaskLabel',
+}
+</script>
+
+<style scoped></style>
