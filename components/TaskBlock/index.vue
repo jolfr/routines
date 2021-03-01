1304daade3c8686475fd27544c0edadfa8a5eb7b
Jolfr <thomas.jack.carroll@gmail.com>
2021-03-01 08:03:41 -0500

    added basic def for task

--- components/TaskBlock/index.vue
+++ components/TaskBlock/index.vue
@@ -1,8 +1,23 @@
-<template><div></div></template>
+<template>
+  <div class="task">
+    <BasicBlock :label="task.name" class="bg-teal-800 text-white" />
+  </div>
+</template>
 
 <script>
+import BasicBlock from '../BasicBlock/index'
+
 export default {
   name: 'TaskBlock',
+  components: [BasicBlock],
+  props: {
+    task: {
+      type: Object,
+      required: true,
+      default: () => ({ name: '', ref: '' }),
+      validator: (task) => ['name', 'ref'].every((key) => key in task),
+    },
+  },
 }
 </script>
 
