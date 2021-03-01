d443f60f82a01407a15903127a28717fb3fb6ef7
Jolfr <thomas.jack.carroll@gmail.com>
2021-03-01 16:35:57 -0500

    added countdown timer

---
+++ components/Timer/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div><BaseTimer :time-left="timeLeft" /></div>
+</template>
+
+<script>
+import BaseTimer from './BaseTimer/index'
+
+export default {
+  name: 'Timer',
+  components: {
+    BaseTimer,
+  },
+  props: {
+    timeLimit: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      timerInterval: null,
+      timePassed: 0,
+    }
+  },
+  computed: {
+    timeLeft() {
+      return this.timeLimit - this.timePassed
+    },
+  },
+  mounted() {
+    this.startTimer()
+  },
+  methods: {
+    startTimer() {
+      this.timerInterval = setInterval(() => (this.timePassed += 1), 1000)
+    },
+  },
+}
+</script>
+
+<style scoped></style>
