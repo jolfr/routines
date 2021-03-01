af0c93b8e70638bd1318ea1125d1ca351169ad19
Jolfr <thomas.jack.carroll@gmail.com>
2021-03-01 14:46:03 -0500

    mocked up basic nav for task view

---
+++ components/TaskNav/index.stories.js
@@ -0,0 +1,29 @@
+import { action } from '@storybook/addon-actions'
+import TaskNav from './index'
+
+export default {
+  title: 'Task Navigation',
+  component: TaskNav,
+  excludeStories: /.*Data$/,
+}
+
+export const actionsData = {
+  onPrev: action('prev-task'),
+  onNext: action('next-task'),
+}
+
+const Template = (args, { argTypes }) => ({
+  components: { TaskNav },
+  props: Object.keys(argTypes),
+  methods: actionsData,
+  template:
+    '<TaskNav v-bind="$props" @prev-task="onPrev" @next-task="onNext" />',
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  task: {
+    title: 'TEST TITLE',
+    desc: 'TEST DESCRIPTION',
+  },
+}
