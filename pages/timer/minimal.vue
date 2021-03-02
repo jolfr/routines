afebfc10a5aae2714c8f5b0425b8c0ac57a41965
Jolfr <thomas.jack.carroll@gmail.com>
2021-03-02 18:01:49 -0500

    added next task slider to page

--- pages/timer/minimal.vue
+++ pages/timer/minimal.vue
@@ -1,14 +1,19 @@
 <template>
-  <div><Timer time-limit="300" /></div>
+  <div>
+    <Timer time-limit="300" />
+    <NextTask />
+  </div>
 </template>
 
 <script>
 import Timer from '@/components/Timer/index'
+import NextTask from '@/components/NextTask/index'
 
 export default {
   name: 'Minimal',
   components: {
     Timer,
+    NextTask,
   },
 }
 </script>
