058e7381fae8bf8cd15751b8138a444c078e2005
Jolfr <thomas.jack.carroll@gmail.com>
2021-03-02 07:03:24 -0500

    added basic next task slider

---
+++ components/NextTask/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <drag-verify
+    :width="width"
+    :height="height"
+    :text="text"
+    :success-text="successText"
+    :background="background"
+    :progress-bar-bg="progressBarBg"
+    :completed-bg="completedBg"
+    :handler-bg="handlerBg"
+    :handler-icon="handlerIcon"
+    :text-size="textSize"
+    :success-icon="successIcon"
+    :circle="getShape"
+  ></drag-verify>
+</template>
+
+<script>
+import dragVerify from 'vue-drag-verify'
+
+export default {
+  name: 'NextTask',
+  components: {
+    dragVerify,
+  },
+}
+</script>
+
+<style scoped></style>
