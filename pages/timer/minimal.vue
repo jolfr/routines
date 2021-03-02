cf7cb78194f87b8a40acd69dcad13a1bcd747c20
Jolfr <thomas.jack.carroll@gmail.com>
2021-03-02 17:05:12 -0500

    added timer to page

--- pages/timer/minimal.vue
+++ pages/timer/minimal.vue
@@ -1,8 +1,15 @@
-<template><div></div></template>
+<template>
+  <div><Timer time-limit="300" /></div>
+</template>
 
 <script>
+import Timer from '@/components/Timer/index'
+
 export default {
   name: 'Minimal',
+  components: {
+    Timer,
+  },
 }
 </script>
 
