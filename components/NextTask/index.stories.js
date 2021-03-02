51aed17286b07e7ef345e8290ed8038b800729b7
Jolfr <thomas.jack.carroll@gmail.com>
2021-03-02 16:06:53 -0500

    added next task mock component and story

---
+++ components/NextTask/index.stories.js
@@ -0,0 +1,16 @@
+import NextTask from './index'
+
+export default {
+  title: 'Next Task',
+  component: NextTask,
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+}
+
+const Template = (args, { argTypes }) => ({
+  components: { NextTask },
+  props: Object.keys(argTypes),
+  template: '<NextTask v-bind="$props"/>',
+})
+
+export const Default = Template.bind({})
---
+++ components/NextTask/index.vue
@@ -0,0 +1,7 @@
+<template><div class="NextTask"></div></template>
+
+<script>
+export default {
+  name: 'NextTask',
+}
+</script>
